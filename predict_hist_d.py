fde355bfb6dd898bc50565e59f77b054be2b680b
fhqlatm <fhqlatm@gmail.com>
2021-10-07 22:42:11 +0900

    update: function

--- predict_hist_d.py
+++ predict_hist_d.py
@@ -4,9 +4,9 @@ import os
 
 def predict_hist_d(train_data):
 
-    train_data=train_data[train_data.hold_d>=14]
+    train_data["hist_d"] = train_data["hold_d"] * 0.6
+    train_data.loc[train_data.hold_d > 14,'hist_d']= train_data.hist_d * 1.1
 
-    train_data["hist_d"] = (train_data["hold_d"] * (0.6))
     train_data.hist_d = np.trunc(train_data["hist_d"])
 
     return train_data
